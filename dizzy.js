31fa1d20e70542678dd2f7a884565e7235adf77e
Moerphy <moerphy@googlemail.com>
2010-10-11 23:54:03 +0200

    Introducing: working support for subgroups. (I hear you like groups, so I put a group in your group so you can transform while you transform)

--- dizzy.js
+++ dizzy.js
@@ -46,11 +46,12 @@ Dizzy.prototype.hasGroupNumber = function(number){
 Dizzy.prototype.getGroups = function(number){
 	return $('g.group_'+number, this.svg.root());
 }
-
+/**
+ * Just returns the internal counter
+ */
 Dizzy.prototype.getShownGroupNumber =  function(){
 	return parseInt(this.groupNum);
 }
-
 
 /**
  * Checks internal counter against element-classes in svg, 
@@ -85,8 +86,6 @@ Dizzy.prototype.show =  function(number){
 }
 
 
-//*/
-
 /**
  * Gets the group that should be displayed and undoes the transformations that have been applied to it.
  * This is done by fetching the Transformationmatrix of the group, inverse it and apply it to the _canvas group_ 
@@ -103,18 +102,42 @@ Dizzy.prototype.updateDisplay =  function(){
 			$(this.canvas).animate({svgTransform:  'matrix(1 0 0 1 0 0)'}, this.transformTime);
 		}else{
 			activeGroup =this.getGroups(this.groupNum);
-			// TODO check for null.. or something like that..
+
 			if( activeGroup.length > 0 ){
 				// get transformationmatrix (SVGMatrix class)
-				var activeGroupTransformBase = activeGroup[0].transform.baseVal;
+				var activeGroupTransformBase = activeGroup[0].transform.baseVal; // TODO multiple nodes with that class?
 				
 				if( activeGroupTransformBase.numberOfItems == 0 ){
-					//activeGroupTransformBase.createSVGTransformFromMatrix();
+					 var newTransform = this.svg.root().createSVGTransform();
+					 activeGroupTransformBase.appendItem(newTransform);
 				}
 				var activeGroupTransformMatrix = activeGroupTransformBase.consolidate().matrix;
-	
+				
+				// subgroups have to be "de-transformed" with the transformations of their parents
+				var parentGroups = $(activeGroup[0]).parentsUntil('g#canvas');
+				
+				/* 
+				 * iterate over parents in the DOM until you hit the #canvas-group
+				 * multiply parent transformation matrix to undo all of them too..
+				 */
+				parentGroups.each(	function(index){
+										var parent = $(this);
+										var parentBase = parent[0].transform.baseVal;
+										if( parentBase.numberOfItems == 0 ){
+											var newTransform = this.svg.root().createSVGTransform();
+											parentBase.appendItem(newTransform);
+										}
+										var parentMatrix = parentBase.consolidate().matrix;
+										// careful with the order of operands on the multiplication
+										// matrix multiplications are not commutative!
+										activeGroupTransformMatrix = parentMatrix.multiply(activeGroupTransformMatrix);
+										
+									});
+				
+				
 				// multiply inverse transformation matrix on canvas (should reverse active transformation)
 				var newMatrix = activeGroupTransformMatrix.inverse();
+				
 				$(this.canvas).animate({svgTransform:  'matrix('+newMatrix.a+' '+newMatrix.b+' '+newMatrix.c+' '+newMatrix.d+' '+newMatrix.e+' '+newMatrix.f+')'}, this.transformTime);
 			}
 		}
@@ -122,15 +145,12 @@ Dizzy.prototype.updateDisplay =  function(){
 	}	
 }
 
-/*
-Dizzy.prototype.getGroupDimension =  function(group){
-	var dimXYWH = {10000,10000,0,0};
-	(function(children, values){
-		for( i = 0; i < children.length; ++i ){
-			
-		}
-	})(group.children(), dimXYWH);
-	arguments.callee();
-}
-*/
 
+
+
+
+
+
+
+
+//I'm sorry, but our princess is in another castle!
