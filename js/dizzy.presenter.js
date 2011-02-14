0b4402b78e603c20fe3ea31a11b7bce662dd7373
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-02-14 16:25:56 +0100

    * presenter URLs not broken anymore

--- js/dizzy.presenter.js
+++ js/dizzy.presenter.js
@@ -1,10 +1,26 @@
 dizz = new Dizzy('#dizzy');
 var inputNumberMode = false;
 
+// Read a page's GET URL variables and return them as an associative array.
+// found at <http://snipplr.com/view/799/get-url-variables/>, modified
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('#')[0].split('&');
+
+    for(var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+
+    return vars;
+}
+
 $(document).ready( 
 
     function(){
-        var filepath = window.location.hash.substr(1);
+        var get = getUrlVars();
+        var filepath = get['file'];
         dizz.load(filepath);
 
         $(document).keydown(function(e) {
